f045cfb45e0202a756f65c5a28078e3635170fda
fenogentov <ShuraF@andex.ru>
2021-06-28 16:41:15 +0300

    HW07 is completed

---
+++ hw07_file_copying/bar.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	TealColor  = "\033[1;36m"
+	ResetColor = "\033[0m"
+)
+
+type Bar struct {
+	charFull string
+	start    string
+	end      string
+	percent  int
+	total    int64
+}
+
+func (bar *Bar) Update(current int64) {
+
+	curPercent := int(current * 100 / bar.total)
+	if ((bar.percent + 2) > curPercent) && (current != 0) {
+		return
+	}
+
+	bar.percent = int(curPercent)
+
+	graph := strings.Repeat(bar.charFull, bar.percent/2)
+	fmt.Printf(TealColor+"\r%s%-50s%s"+ResetColor+"%3d%% %10d/%d", bar.start, graph, bar.end, bar.percent, current, bar.total)
+
+}
+func (bar *Bar) Finish() {
+	fmt.Println()
+}
+
+func NewBar(t int64) *Bar {
+	return &Bar{
+		charFull: "█",
+		start:    "[",
+		end:      "]",
+		percent:  0,
+		total:    t,
+	}
+}
