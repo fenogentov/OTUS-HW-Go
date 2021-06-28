6a2726f9946dd70283f9036d380103270bc46587
fenogentov <ShuraF@andex.ru>
2021-06-28 15:34:31 +0300

    HW04 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,16 +11,56 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mx       sync.Mutex
 }
 
 type cacheItem struct {
 	key   string
 	value interface{}
+}
+
+func (c *lruCache) Set(k Key, v interface{}) bool {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+
+	item := &cacheItem{key: string(k), value: v}
+
+	if el, ok := c.items[k]; ok {
+		c.queue.MoveToFront(el)
+		el.Value.(*cacheItem).value = v
+		return true
+	}
+
+	if c.queue.Len() == c.capacity {
+		r := c.queue.Back().Value.(*cacheItem).key
+		delete(c.items, Key(r))
+		c.queue.Remove(c.queue.Back())
+	}
+
+	e := c.queue.PushFront(item)
+	c.items[k] = e
+
+	return false
+}
+
+func (c *lruCache) Get(k Key) (interface{}, bool) {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+
+	if el, ok := c.items[k]; ok {
+		c.queue.MoveToFront(el)
+		return el.Value.(*cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
