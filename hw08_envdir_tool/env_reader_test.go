2bc8793f90a2e807fd421e0700dfba4b81455908
fenogentov <ShuraF@andex.ru>
2021-06-29 09:22:40 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	dirEnv := "./testdata/env"
+	errDirEnv := "./errdata/env"
+
+	t.Run("env read", func(t *testing.T) {
+		env, err := ReadDir(dirEnv)
+		require.Equal(t, err, nil, "ReadDir return error: %s", err)
+		require.Equal(t, env["BAR"].Value, "bar", "error read BAR")
+		require.Equal(t, env["EMPTY"].Value, "", "error read EMPTY")
+		require.Equal(t, env["FOO"].Value, "   foo\nwith new line", "error read FOO")
+		require.Equal(t, env["HELLO"].Value, "\"hello\"", "error read HELLO")
+		require.Equal(t, env["UNSET"].Value, "", "error read UNSET")
+	})
+
+	t.Run("error directory env", func(t *testing.T) {
+		_, err := ReadDir(errDirEnv)
+		require.EqualErrorf(t, err, "open "+errDirEnv+": no such file or directory", "actual %s:", err)
+	})
 }
